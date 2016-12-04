52d6bb7443644b09427a0e20b10439eb96b11875
kellyt1 <tossy.kelly@gmail.com>
2016-12-04 20:17:00 +0000

    first commit

--- src/main/java/org/mitre/web/Application.java
+++ src/main/java/org/mitre/web/Application.java
@@ -1,19 +1,3 @@
-/*******************************************************************************
- * Copyright 2014 The MITRE Corporation
- *   and the MIT Kerberos and Internet Trust Consortium
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
 package org.mitre.web;
 
 import java.security.Principal;
@@ -32,14 +16,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Handles requests for the application home page.
- */
+@SpringBootApplication
+public class Application {
+    ApplicationContext context = new ClassPathXmlApplicationContext("servlet-context.xml");
+    
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+        System.out.println("app starteds");
+    }
+}
+
+
 @Controller
-public class HomeController {
+ class HomeTestController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	// filter reference so we can get class names and things like that.
 	@Autowired
